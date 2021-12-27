27b732d157c8a7dc9a0943fbd3423a2b198d2032
vikrantpahwa <vikrantpahwa0@gmail.com>
2021-12-27 06:07:12 -0800

    Project without Databse

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const lodash = require('lodash');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -14,15 +15,44 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+let posts=[];
 
 app.get("/",function(req,res){
-  res.render("home",{homeStartingContent:homeStartingContent});
+    res.render("home",{homeStartingContent:homeStartingContent,posts:posts});
 })
 app.get("/about",function(req,res){
   res.render("about",{aboutContent:aboutContent});
 })
 app.get("/contact",function(req,res){
   res.render("contact",{contactContent:contactContent});
+})
+app.get("/compose",function(req,res){
+  res.render("compose");
+})
+
+app.get("/post",function(req,res){
+  res.render("post",{title:"post.title",publishMessage:"post.publishMessage"});
+})
+
+app.get("/posts/:title",function(req,res){
+
+  posts.forEach(function(post){
+    //Used for lowercasing all capitals and putting spaces for the kebab casing.
+    if(lodash.lowerCase(req.params.title)===lodash.lowerCase(post.title)){
+      res.render("post",{title:post.title,publishMessage:post.publishMessage});
+    }
+  })
+
+})
+app.post("/compose",function(req,res){
+  let valuesRecieved={
+
+  title:req.body.title,
+  publishMessage:req.body.publishMessage
+
+  };
+  posts.push(valuesRecieved);
+  res.redirect("/")
 })
 app.listen(3000, function() {
   console.log("Server started on port 3000");
